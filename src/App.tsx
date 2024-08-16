fcdaaeaedc8bf2b61415301f3cf54b7ae5a4430e
Sanda Mandresy <98638893+Sanmandresy@users.noreply.github.com>
2024-08-16 15:02:19 +0300

    chore: configure notistack provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { SnackbarProvider } from "notistack"
 import { AppRoutes } from "./routes"
 
+const SNACKBAR_DENSE = true
+const MAX_SNACK = 3
+
 function App() {
   return (
     <>
       <AppRoutes />
-      <SnackbarProvider />
+      <SnackbarProvider
+        dense={SNACKBAR_DENSE}
+        anchorOrigin={{
+          horizontal: "right",
+          vertical: "bottom",
+        }}
+        maxSnack={MAX_SNACK}
+      />
     </>
   )
 }
