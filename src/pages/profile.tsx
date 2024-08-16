80b1d24f7363040b51a23cccda9763266a910715
Sanda Mandresy <98638893+Sanmandresy@users.noreply.github.com>
2024-08-16 22:55:01 +0300

    feat(not-implemented): add profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,3 +1,7 @@
 export const Profile = () => {
-  return <section>profile</section>
+  return (
+    <section className="min-h-screen p-2">
+      <h2 className="py-2 text-xl quicksand-bold ">Profile</h2>
+    </section>
+  )
 }
